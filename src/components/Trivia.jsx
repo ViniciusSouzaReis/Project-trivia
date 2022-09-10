bb6eaa8aeeb21aa25ef9889736d71d6fd7fdc8c3
Vinicius Luiz de Souza Reis <vinniiesouza@gmail.com>
2022-09-10 11:51:35 -0300

    Requisitos 7 e 8

--- src/components/Trivia.jsx
+++ src/components/Trivia.jsx
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import store from '../redux';
+import '../trivia.css';
+
+let clock;
 
 class Trivia extends Component {
   constructor() {
@@ -10,6 +13,10 @@ class Trivia extends Component {
       trivia: [],
       counter: 0,
       array: [],
+      wrong: '',
+      correct: '',
+      disabled: false,
+      time: 30,
     };
   }
 
@@ -21,6 +28,7 @@ class Trivia extends Component {
       array: [state.game.trivia.results[counter].correct_answer,
         ...state.game.trivia.results[counter].incorrect_answers],
     });
+    this.Clock();
   }
 
   handleClick = () => {
@@ -31,10 +39,42 @@ class Trivia extends Component {
         counter: estadoAnterior.counter + 1,
       }));
     }
+    this.Clock();
+  };
+
+  Clock = () => {
+    const ONE_SECOND = 1000;
+    clock = setInterval(() => {
+      this.setState((prev) => ({
+        time: prev.time - 1,
+        disabled: false,
+      }));
+    }, ONE_SECOND);
+    this.breakClock(clock);
+  };
+
+  breakClock = (param) => {
+    const THIRTY_SECOND = 30000;
+    setTimeout(() => {
+      clearInterval(param);
+      this.setState({
+        disabled: true,
+        time: 30,
+      });
+    }, THIRTY_SECOND);
+  };
+
+  answersFunction = () => {
+    clearInterval(clock);
+    this.setState({
+      wrong: 'wrong-answer-css ',
+      correct: 'correct-answer-css',
+      disabled: true,
+    });
   };
 
   render() {
-    const { trivia, counter, array } = this.state;
+    const { trivia, counter, array, wrong, correct, disabled, time } = this.state;
     const ZERO_FIVE = 0.5;
     const ONE = 1;
     const array2 = array.map((e, index) => {
@@ -50,7 +90,6 @@ class Trivia extends Component {
       };
     });
     array2.sort(() => ((Math.random() > ZERO_FIVE) ? ONE : -ONE));
-    console.log(array2);
     return (
       <div>
         {(trivia.length !== 0
@@ -58,12 +97,16 @@ class Trivia extends Component {
                   <div>
                     <p data-testid="question-category">{trivia[counter].category}</p>
                     <p data-testid="question-text">{trivia[counter].question}</p>
+                    <span>{ time }</span>
                     <section data-testid="answer-options">
                       {array2.map((e, index) => (
                         e.valor === true ? (
                           <button
                             type="button"
+                            className={ correct }
                             data-testid="correct-answer"
+                            disabled={ disabled }
+                            onClick={ this.answersFunction }
                             key={ 5 }
                           >
                             {e.resposta}
@@ -72,7 +115,10 @@ class Trivia extends Component {
                             <button
                               type="button"
                               data-testid={ ` wrong-answer-${index}` }
+                              onClick={ this.answersFunction }
+                              className={ wrong }
                               key={ index }
+                              disabled={ disabled }
                             >
                               {e.reposta}
                             </button>
